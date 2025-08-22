f22b39968fa732a79d08fc87ea78b2f4ffc64ab3
Jonathan Alvarez <2167222+jonalvarezz@users.noreply.github.com>
2025-08-22 01:01:36 -0500

    clean up schema

--- stateful-l2-wrapped-m-token/src/wm-mappings-l2.ts
+++ stateful-l2-wrapped-m-token/src/wm-mappings-l2.ts
@@ -14,7 +14,6 @@ import {
     LatestUpdateTimestampSnapshot,
     Migration,
     MToken,
-    PrincipalOfTotalEarningSupplySnapshot,
     ReceivedSnapshot,
     SentSnapshot,
     TotalBurnedSnapshot,
